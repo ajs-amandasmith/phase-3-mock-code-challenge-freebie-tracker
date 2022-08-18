e85eb1d3703921c5d3f55a769623747acb38651e
Amanda Smith <ajs.amandasmith@gmail.com>
2022-08-18 15:51:04 -0500

    commit tree fceccbbb9f95d28cd2c5cc4cf2040a8b82543ac4

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,22 @@
 class Company < ActiveRecord::Base
+  has_many :freebies
+  has_many :devs, through: :freebies
+
+  def give_freebie(dev, item_name, value)
+    Freebie.create(dev_id: dev.id, item_name: item_name, value: value, company_id: self.id)
+  end
+
+  def self.oldest_year
+    Company.minimum(:founding_year)
+  end
+
+  def self.oldest_company
+    Company.find_by(founding_year: Company.oldest_year)
+  end
+
+  # SOLUTION
+  # def self.oldest_company
+  #   Company.all.order(:founding_year).first
+  # end
 
 end
